0f3ef7ff3fad57b6a61d9700fe707ad0a97c844e
hyzboy <hyzboy@gmail.com>
2020-10-25 15:07:01 +0800

    add GetMipLevel functions.

--- inc/hgl/TypeFunc.h
+++ inc/hgl/TypeFunc.h
@@ -472,6 +472,21 @@ namespace hgl
         return(value);
     }
 
+    inline const uint GetMipLevel(const uint size)
+    {
+        return static_cast<uint>(floor(log2(size)))+1;
+    }
+
+    inline const uint GetMipLevel(const uint width,const uint height)
+    {
+        return GetMipLevel(hgl_max(width,height));
+    }
+
+    inline const uint GetMipLevel(const uint width,const uint height,const uint depth)
+    {
+        return GetMipLevel(hgl_max(hgl_max(width,height),depth));
+    }
+
     /**
      * 角度转弧度
      */
