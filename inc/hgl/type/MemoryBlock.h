ec44cb756e67b39fd5a7ff990f474af9272a948f
hyzboy <hyzboy@gmail.com>
2021-03-10 14:26:04 +0800

    layout codes.

--- inc/hgl/type/MemoryBlock.h
+++ inc/hgl/type/MemoryBlock.h
@@ -19,19 +19,19 @@ namespace hgl
 
     public:
 
-        MemoryBlock(AbstractMemoryAllocator *ama);
+        MemoryBlock(AbstractMemoryAllocator *ama=new MemoryAllocator);
         virtual ~MemoryBlock();
 
         virtual const   uint64  GetSize ()const                 {return memory_allocator->GetSize();}
         virtual         void *  Get     ()                      {return memory_allocator->Get();}
         virtual         void *  Get     (const uint64 offset)   {return memory_allocator->Get(offset);}
 
-        virtual         void Clear      ();
-        virtual         bool Alloc      (const uint64 size,const uint64 uint_size=0);
-        virtual         bool Write      (const uint64 target,const void *source,const uint64 size);
-        virtual         bool Write      (const uint64 target,MemoryBlock *source,const uint64 offset,const uint64 size);
-        virtual         bool Exchange   (const uint64 target,const uint64 source,const uint64 size);
-        virtual         void Move       (const uint64 target,const uint64 source,const uint64 size);
+        virtual         void    Clear   ();
+        virtual         bool    Alloc   (const uint64 size,const uint64 uint_size=0);
+        virtual         bool    Write   (const uint64 target,const void *source,const uint64 size);
+        virtual         bool    Write   (const uint64 target,MemoryBlock *source,const uint64 offset,const uint64 size);
+        virtual         bool    Exchange(const uint64 target,const uint64 source,const uint64 size);
+        virtual         void    Move    (const uint64 target,const uint64 source,const uint64 size);
     };//class MemoryBlock:public AbstractDataBlock
 }//namespace hgl
 #endif//HGL_MEMORY_BLOCK_INCLUDE
