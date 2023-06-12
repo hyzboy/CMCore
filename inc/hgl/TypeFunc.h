51650bd4b120bbbe1a969d0b66ec300f59a00f43
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-06-12 15:38:21 +0800

    added RANGE_CHECK_RETURN_ZERO

--- inc/hgl/TypeFunc.h
+++ inc/hgl/TypeFunc.h
@@ -29,9 +29,10 @@ namespace hgl
                       value<=(ctype)ECTYPE::END_RANGE;  \
                       value++)
 
-    #define RANGE_CHECK_RETURN(value,return_value)       if(!RangeCheck(value))return(return_value);
-    #define RANGE_CHECK_RETURN_FALSE(value)              if(!RangeCheck(value))return(false);
-    #define RANGE_CHECK_RETURN_NULLPTR(value)            if(!RangeCheck(value))return(nullptr);
+    #define RANGE_CHECK_RETURN(value,return_value)      if(!RangeCheck(value))return(return_value);
+    #define RANGE_CHECK_RETURN_ZERO(value)              if(!RangeCheck(value))return(0);
+    #define RANGE_CHECK_RETURN_FALSE(value)             if(!RangeCheck(value))return(false);
+    #define RANGE_CHECK_RETURN_NULLPTR(value)           if(!RangeCheck(value))return(nullptr);
 
     #define HGL_CONVER_TO_MEM_ALIGN(x)      ((((x)+HGL_MEM_ALIGN-1)/HGL_MEM_ALIGN)*HGL_MEM_ALIGN)                   //内存对齐转换宏
 
