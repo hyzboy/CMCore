ab0292d27e9825f1259688f78cf3bd02fbc9e785
hyzboy <hyzboy@gmail.com>
2023-10-05 00:49:34 +0800

    update TypeFunc.h

--- inc/hgl/TypeFunc.h
+++ inc/hgl/TypeFunc.h
@@ -12,9 +12,9 @@ namespace hgl
     #define HGL_OFFICAL_WEB_URL_u8 U8_TEXT("http://www.hyzgame.com")
     #define HGL_OFFICAL_WEB_URL_OS OS_TEXT("http://www.hyzgame.com")
 
-    #define HGL_COPYRIGHT_STRING            "(C)Copyright 2022 www.hyzgame.com"
-    #define HGL_COPYRIGHT_STRING_U8 U8_TEXT("(C)Copyright 2022 www.hyzgame.com")
-    #define HGL_COPYRIGHT_STRING_OS OS_TEXT("(C)Copyright 2022 www.hyzgame.com")
+    #define HGL_COPYRIGHT_STRING            "(C)Copyright 2023 www.hyzgame.com"
+    #define HGL_COPYRIGHT_STRING_U8 U8_TEXT("(C)Copyright 2023 www.hyzgame.com")
+    #define HGL_COPYRIGHT_STRING_OS OS_TEXT("(C)Copyright 2023 www.hyzgame.com")
 
     #define ENUM_CLASS_RANGE(begin,end)     BEGIN_RANGE=begin,END_RANGE=end,RANGE_SIZE=(END_RANGE-BEGIN_RANGE)+1
 
@@ -137,34 +137,34 @@ namespace hgl
         return -1;
     }
 
-    constexpr uint      HGL_SIZE_1KB    =1024;
-    constexpr uint      HGL_SIZE_1MB    =HGL_SIZE_1KB*1024;
-    constexpr uint      HGL_SIZE_1GB    =HGL_SIZE_1MB*1024;
-    constexpr uint64    HGL_SIZE_1TB    =HGL_SIZE_1GB*1024ULL;
-    constexpr uint64    HGL_SIZE_1PB    =HGL_SIZE_1TB*1024ULL;
-    constexpr uint64    HGL_SIZE_1EB    =HGL_SIZE_1PB*1024ULL;
-//  constexpr uint128    HGL_SIZE_1ZB   =HGL_SIZE_1EB*1024ULL;
-//  constexpr uint128    HGL_SIZE_1YB   =HGL_SIZE_1ZB*1024ULL;
+    constexpr const uint      HGL_SIZE_1KB    =1024;
+    constexpr const uint      HGL_SIZE_1MB    =HGL_SIZE_1KB*1024;
+    constexpr const uint      HGL_SIZE_1GB    =HGL_SIZE_1MB*1024;
+    constexpr const uint64    HGL_SIZE_1TB    =HGL_SIZE_1GB*1024ULL;
+    constexpr const uint64    HGL_SIZE_1PB    =HGL_SIZE_1TB*1024ULL;
+    constexpr const uint64    HGL_SIZE_1EB    =HGL_SIZE_1PB*1024ULL;
+//  constexpr const uint128    HGL_SIZE_1ZB   =HGL_SIZE_1EB*1024ULL;
+//  constexpr const uint128    HGL_SIZE_1YB   =HGL_SIZE_1ZB*1024ULL;
 
-    constexpr uint8     HGL_U8_MAX      =0xFF;
-    constexpr uint16    HGL_U16_MAX     =0xFFFF;
-    constexpr uint32    HGL_U32_MAX     =0xFFFFFFFF;
-    constexpr uint64    HGL_U64_MAX     =0xFFFFFFFFFFFFFFFFULL;
+    constexpr const uint8     HGL_U8_MAX      =0xFF;
+    constexpr const uint16    HGL_U16_MAX     =0xFFFF;
+    constexpr const uint32    HGL_U32_MAX     =0xFFFFFFFF;
+    constexpr const uint64    HGL_U64_MAX     =0xFFFFFFFFFFFFFFFFULL;
 
-    constexpr int8      HGL_S8_MAX      =0x7F;
-    constexpr int16     HGL_S16_MAX     =0x7FFF;
-    constexpr int32     HGL_S32_MAX     =0x7FFFFFFF;
-    constexpr int64     HGL_S64_MAX     =0x7FFFFFFFFFFFFFFFLL;
+    constexpr const int8      HGL_S8_MAX      =0x7F;
+    constexpr const int16     HGL_S16_MAX     =0x7FFF;
+    constexpr const int32     HGL_S32_MAX     =0x7FFFFFFF;
+    constexpr const int64     HGL_S64_MAX     =0x7FFFFFFFFFFFFFFFLL;
 
-    constexpr int8      HGL_S8_MIN      =(-0x80);
-    constexpr int16     HGL_S16_MIN     =(-0x8000);
+    constexpr const int8      HGL_S8_MIN      =(-0x80);
+    constexpr const int16     HGL_S16_MIN     =(-0x8000);
 
 #ifdef _MSC_VER
-    constexpr int32     HGL_S32_MIN     =(-0x80000000i32);
-    constexpr int64     HGL_S64_MIN     =(-0x8000000000000000i64);
+    constexpr const int32     HGL_S32_MIN     =(-0x80000000i32);
+    constexpr const int64     HGL_S64_MIN     =(-0x8000000000000000i64);
 #else
-    constexpr int32     HGL_S32_MIN     =(-0x80000000L);
-    constexpr int64     HGL_S64_MIN     =(-0x8000000000000000LL);
+    constexpr const int32     HGL_S32_MIN     =(-0x80000000L);
+    constexpr const int64     HGL_S64_MIN     =(-0x8000000000000000LL);
 #endif//_MSC_VER
 
     template<typename T> T HGL_INTEGER_MAX();
