1a61053e33ba573fb80a3775200e3db83dafd6bd
hyzboy <hyzboy@gmail.com>
2024-07-29 13:45:42 +0800

    changed AxisVector design

--- inc/hgl/math/Vector.h
+++ inc/hgl/math/Vector.h
@@ -39,9 +39,12 @@ namespace hgl
 
 #undef DEF_VECTOR
 
-    constexpr const Vector3f XAxisVector=Vector3f(1,0,0);
-    constexpr const Vector3f YAxisVector=Vector3f(0,1,0);
-    constexpr const Vector3f ZAxisVector=Vector3f(0,0,1);
+    namespace AxisVector
+    {
+        constexpr const Vector3f X=Vector3f(1,0,0);
+        constexpr const Vector3f Y=Vector3f(0,1,0);
+        constexpr const Vector3f Z=Vector3f(0,0,1);
+    };
 
     inline const Vector3f GetAxisVector(const AXIS &axis)
     {
