aa88dec2bae4eec0b42f325ac9aa8e1d7f7c3509
hyzboy <hyzboy@gmail.com>
2025-09-06 08:41:21 +0800

    删除不再使用的Log函数

--- inc/hgl/log/LogInfo.h
+++ inc/hgl/log/LogInfo.h
@@ -13,23 +13,6 @@ namespace hgl
         
         void Log(LogLevel level,const u16char *str,int size);
         void Log(LogLevel level,const u8char *str,int size);
-
-        inline  void Log(LogLevel ll,const U16String &str)
-        {
-            Log(ll,str.c_str(),str.Length());
-        }
-
-        inline  void Log(LogLevel ll,const U8String &str)
-        {
-            Log(ll,str.c_str(),str.Length());
-        }
-
-        #ifdef HGL_SUPPORT_CHAR8_T
-        inline  void Log(LogLevel ll,const AnsiString &str)
-        {
-            std::cout<<str.c_str()<<std::endl;
-        }
-        #endif//HGL_SUPPORT_CHAR8_T
     }//namespace logger
 
     using namespace logger;
