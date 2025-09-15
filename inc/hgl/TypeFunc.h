7314a875bde754e3d14bcdbe2f59048970cb1940
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-09-16 00:43:22 +0800

    增加ArrayWriter辅助模板类

--- inc/hgl/TypeFunc.h
+++ inc/hgl/TypeFunc.h
@@ -530,4 +530,29 @@ namespace hgl
     {
         hgl_free(items);
     }
+
+    template<typename C,typename V> struct ArrayWriter
+    {
+        C *count;
+        V *values;
+
+    public:
+
+        ArrayWriter(C *c,V *v)
+        {
+            count = c;
+            values = v;
+
+            *count = 0;
+        }
+
+        ArrayWriter &operator <<(const V &v)
+        {
+            *values = v;
+            ++values;
+            ++(*count);
+
+            return *this;
+        }
+    };
 }//namespace hgl
