f8e2ca907cc1e8b5264c4e2a605304aaaa9cf4a6
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-09-21 00:29:48 +0800

    WindowsEvent增加Create事件，并在WindowEvent中记录当前窗体大小

--- inc/hgl/io/event/WindowEvent.h
+++ inc/hgl/io/event/WindowEvent.h
@@ -1,67 +1,76 @@
-﻿#ifndef HGL_IO_WINDOW_EVENT_INCLUDE
-#define HGL_IO_WINDOW_EVENT_INCLUDE
+﻿#pragma once
 
 #include<hgl/io/event/EventDispatcher.h>
-namespace hgl
+#include<hgl/math/Vector.h>
+
+namespace hgl::io
 {
-    namespace io
+    enum class WindowEventID
     {
-        enum class WindowEventID
+        Create,
+        Active,
+        Resize,
+        Close
+    };//enum class WindowEventID
+
+    union WindowEventData
+    {
+        uint64 data;
+
+        struct
         {
-            Active,
-            Resize,
-            Close
-        };//enum class WindowEventID
-
-        union WindowEventData
-        {
-            uint64 data;
-
-            struct
-            {
-                uint16 width,height;
-            };
-
-            bool active;
+            uint16 width,height;
         };
 
-        constexpr size_t WindowEventDataBytes=sizeof(WindowEventData);
+        bool active;
+    };
 
-        class WindowEvent:public EventDispatcher
+    constexpr size_t WindowEventDataBytes=sizeof(WindowEventData);
+
+    class WindowEvent:public EventDispatcher
+    {
+        WindowEventData *wed=nullptr;
+
+        Vector2i WindowSize{};
+
+    public:
+
+        WindowEvent():EventDispatcher(InputEventSource::Window){}
+        virtual ~WindowEvent()=default;
+
+        const Vector2i &GetWindowSize()const{return WindowSize;}
+
+        virtual EventProcResult OnEvent(const EventHeader &header,const uint64 data) override
         {
-            WindowEventData *wed;
-
-        public:
-
-            WindowEvent():EventDispatcher(InputEventSource::Window){wed=nullptr;}
-            virtual ~WindowEvent()=default;
-
-            virtual EventProcResult OnEvent(const EventHeader &header,const uint64 data) override
+            if(header.type==InputEventSource::Window)
             {
-                if(header.type==InputEventSource::Window)
+                wed=(WindowEventData *)&data;
+
+                switch(WindowEventID(header.id))
                 {
-                    wed=(WindowEventData *)&data;
+                    case WindowEventID::Active: OnActive(wed->active);break;
+                    case WindowEventID::Close:  OnClose();break;
+                    case WindowEventID::Resize: WindowSize.x = wed->width;
+                                                WindowSize.y = wed->height;
+                                                OnResize(wed->width,wed->height);break;
 
-                    switch(WindowEventID(header.id))
-                    {
-                        case WindowEventID::Active:OnActive  (wed->active)           ;break;
-                        case WindowEventID::Resize:OnResize  (wed->width,wed->height);break;
-                        case WindowEventID::Close: OnClose   ()                      ;break;
-                    }
+                    case WindowEventID::Create: WindowSize.x = wed->width;
+                                                WindowSize.y = wed->height;
+                                                OnCreate(wed->width,wed->height);break;
                 }
-
-                if(EventDispatcher::OnEvent(header,data)==EventProcResult::Break)
-                    return EventProcResult::Break;
-
-                return EventProcResult::Continue;
             }
 
-        public:
+            if(EventDispatcher::OnEvent(header,data)==EventProcResult::Break)
+                return EventProcResult::Break;
 
-            virtual void OnResize(uint w,uint h){}
-            virtual void OnActive(bool){}
-            virtual void OnClose (){}
-        };//class WindowEvent:public EventDispatcher
-    }//namespace io
-}//namespace hgl
-#endif//HGL_IO_WINDOW_EVENT_INCLUDE
+            return EventProcResult::Continue;
+        }
+
+    public:
+
+        virtual void OnCreate(uint w,uint h){}
+        virtual void OnResize(uint w,uint h){}
+        virtual void OnActive(bool){}
+        virtual void OnClose (){}
+    };//class WindowEvent:public EventDispatcher
+}//namespace hgl::io
