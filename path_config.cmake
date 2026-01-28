dfbc1a4dfa82bd215ac16949db512c00ca43202f
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-28 20:58:52 +0800

    改进cmake路径配置

--- path_config.cmake
+++ path_config.cmake
@@ -1,9 +1,8 @@
 macro(CMCoreSetup CMCORE_ROOT_PATH)
+    message("CMCore root path: " ${CMCORE_ROOT_PATH})
 
-    message("CMROOT_ROOT_PATH: " ${CMCORE_ROOT_PATH})
-
-    set(CMCORE_ROOT_INCLUDE_PATH    ${CMCORE_ROOT_PATH}/inc)
-    set(CMCORE_ROOT_SOURCE_PATH     ${CMCORE_ROOT_PATH}/src)
+    set(CMCORE_ROOT_INCLUDE_PATH ${CMCORE_ROOT_PATH}/inc)
+    set(CMCORE_ROOT_SOURCE_PATH ${CMCORE_ROOT_PATH}/src)
 
     include_directories(${CMCORE_ROOT_INCLUDE_PATH})
 endmacro()
