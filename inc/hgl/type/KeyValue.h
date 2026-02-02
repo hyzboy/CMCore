22a27c2f77e90b73755fd2406b28fa0c1448fde5
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-02-02 11:55:17 +0800

    增加==/!=操作符重载

--- inc/hgl/type/KeyValue.h
+++ inc/hgl/type/KeyValue.h
@@ -9,5 +9,15 @@ namespace hgl
         K key;
         V value;
         uint64_t hash; // pre-computed hash for faster comparison
+
+        bool operator==(const KeyValue& other) const
+        {
+            return key == other.key && value == other.value;
+        }
+
+        bool operator!=(const KeyValue& other) const
+        {
+            return !(*this == other);
+        }
     };
 }//namespace hgl
